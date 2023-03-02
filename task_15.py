99ac2307aefe487ba32b95f4f7fc1c0883510257
Dan <innocoffee@ya.ru>
2023-03-02 12:55:14 +0300

    Add task 15

---
+++ task_15.py
@@ -0,0 +1,39 @@
+import itertools
+
+# region: input
+ПЕРЕМЕННЫЕ = "x"  # Список всех переменных без "A"
+
+
+def ФУНКЦИЯ(x: int, A: int) -> bool:
+    """Логическая функция из условия"""
+    return x & 51 == 0 or ((x & A == 0) ** (x & 41 == 0))
+
+
+НАЙТИ_МАКСИМАЛЬНОЕ = True  # True если нужно найти наибольшее A
+
+
+# endregion: input
+# region: logic
+
+
+def main():
+    maximum = 0
+    for A in range(1, 1000):
+        for variables in itertools.product(range(1000), repeat=len(ПЕРЕМЕННЫЕ)):
+            if not ФУНКЦИЯ(*variables, A=A):
+                break
+        else:
+            if not НАЙТИ_МАКСИМАЛЬНОЕ:
+                print("Answer:", A)
+                break
+            else:
+                maximum = max(maximum, A)
+
+    if НАЙТИ_МАКСИМАЛЬНОЕ:
+        print("Answer:", maximum)
+
+
+# endregion: logic
+
+if __name__ == "__main__":
+    main()
--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
-import task_2, task_19
+import task_2
+import task_15
+import task_19
+
 
 def test_task_19(capfd):
     # sourcery skip: extract-duplicate-method, move-assign-in-block
@@ -26,12 +29,9 @@ def test_task_19(capfd):
 
 def test_task_2(capfd):
     # sourcery skip: extract-duplicate-method, move-assign-in-block
-    task_2.ИЗВЕСТНЫЕ = [
-        [..., 0, 0, 0],
-        [..., 0, ..., 0]
-    ]
+    task_2.ИЗВЕСТНЫЕ = [[..., 0, 0, 0], [..., 0, ..., 0]]
     task_2.ПЕРЕМЕННЫЕ = "xyz"
-    task_2.ФУНКЦИЯ = lambda x, y, z: int((z == x) ** (x or y))
+    task_2.ФУНКЦИЯ = lambda x, y, z: (z == x) ** (x or y)
     task_2.main()
     out = capfd.readouterr()[0].strip()
     assert out == "Answer: xzy"
@@ -42,7 +42,7 @@ def test_task_2(capfd):
         [..., ..., 0, 0, 0],
         [..., 0, ..., ..., 0],
     ]
-    task_2.ФУНКЦИЯ = lambda x, y, z, w: int((((not y) ** w) and x ** y) ** ((not x) == (not not z)))
+    task_2.ФУНКЦИЯ = lambda x, y, z, w: (((not y) ** w) and x ** y) ** (x != z)
     task_2.main()
     out = capfd.readouterr()[0].strip()
     assert out == "Answer: yzxw"
@@ -53,9 +53,38 @@ def test_task_2(capfd):
         [1, ..., ..., ..., 0],
         [..., 1, ..., 1, 0],
     ]
-    task_2.ФУНКЦИЯ = lambda x, y, z, w: int(((y ** x) and (w ** y)) or (z == (x or y)))
+    task_2.ФУНКЦИЯ = lambda x, y, z, w: ((y**x) and (w**y)) or (z == (x or y))
     task_2.main()
     out = capfd.readouterr()[0].strip()
     assert out == "Answer: ywzx"
 
 
+def test_task_15(capfd):
+    # sourcery skip: extract-duplicate-method, move-assign-in-block
+    task_15.ПЕРЕМЕННЫЕ = "x"
+    task_15.ФУНКЦИЯ = lambda x, A: ((x & A != 0) ** (x & 12 == 0)) ** (x & 29 != 0)
+    task_15.НАЙТИ_МАКСИМАЛЬНОЕ = False
+    task_15.main()
+    out = capfd.readouterr()[0].strip()
+    assert out == "Answer: 17"
+
+    task_15.ПЕРЕМЕННЫЕ = "x"
+    task_15.ФУНКЦИЯ = lambda x, A: ((x & A != 0) ** (x & 17 == 0)) ** (x & 25 != 0)
+    task_15.НАЙТИ_МАКСИМАЛЬНОЕ = False
+    task_15.main()
+    out = capfd.readouterr()[0].strip()
+    assert out == "Answer: 8"
+
+    task_15.ПЕРЕМЕННЫЕ = "x"
+    task_15.ФУНКЦИЯ = lambda x, A: ((x & A != 0) ** (x & 33 == 0)) ** (x & 41 != 0)
+    task_15.НАЙТИ_МАКСИМАЛЬНОЕ = False
+    task_15.main()
+    out = capfd.readouterr()[0].strip()
+    assert out == "Answer: 8"
+
+    task_15.ПЕРЕМЕННЫЕ = "x"
+    task_15.ФУНКЦИЯ = lambda x, A: x & 51 == 0 or ((x & A == 0) ** (x & 41 == 0))
+    task_15.НАЙТИ_МАКСИМАЛЬНОЕ = True
+    task_15.main()
+    out = capfd.readouterr()[0].strip()
+    assert out == "Answer: 41"
